8c0eed6ab6962eeb250b81c5fe2aef5485215cd5
dsw8t2 <dweissb252@hotmail.com>
2018-05-07 00:26:27 -0500

    more updates

--- CapitalsPenguins_stats.sql
+++ CapitalsPenguins_stats.sql
@@ -20,3 +20,6 @@ CREATE TABLE IF NOT EXISTS `stats` (
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 INSERT INTO `stats` (`Name`, `Team`, `GP`, `G`, `A`, `P`, `+/-`, `PIM`, `PPG`, `PPP`, `SHG`, `SHP`, `GWG`, `OTG`, `Shots`) VALUES
+('Alex Ovechkin','WSH',82,49,38,87,3,32,17,31,0,0,7,3,355),
+('Evgeny Kuznetsov','WSH',79,27,56,83,3,48,7,30,1,1,8,2,187),
+('Nicklas Backstrom','WSH',81,21,50,71,5,46,7,26,0,0,4,1,165),
