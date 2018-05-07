791e3057a5e0cf295c8e854ff30f082ed3f3dbcd
Sahand Sadri <ssyxf@mail.missouri.edu>
2018-05-07 00:54:11 -0500

    updated links

--- PenguinsCapitals.php
+++ PenguinsCapitals.php
@@ -19,7 +19,7 @@
 <body>
 	<!-- DO NOT CHANGE ANYTHING FOR NAV BAR -->
 	<nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
-	  <a class="navbar-brand" href="#">NHL Rivalries</a>
+	  <a class="navbar-brand" href="index.html">NHL Rivalries</a>
 	  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 	    <span class="navbar-toggler-icon"></span>
 	  </button>
