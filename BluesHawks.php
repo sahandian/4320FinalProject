b4aecc15b46baff63c18d84ba4fd80e4290e6bc9
Sahand Sadri <ssyxf@mail.missouri.edu>
2018-05-06 22:14:06 -0500

    blueshawks.php

---
+++ BluesHawks.php
@@ -0,0 +1,256 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>NHL Rivalries</title>
+
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
+
+	<script
+	  src="https://code.jquery.com/jquery-3.2.1.min.js"
+	  integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4="
+	  crossorigin="anonymous"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js"></script> 
+<style>
+	body {
+	padding-top: 70px; 
+	}
+</style>
+</head>
+<body>
+	<!-- DO NOT CHANGE ANYTHING FOR NAV BAR -->
+	<nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
+	  <a class="navbar-brand" href="#">NHL Rivalries</a>
+	  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+	    <span class="navbar-toggler-icon"></span>
+	  </button>
+
+	  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+	    <ul class="navbar-nav mr-auto">
+	      <li class="nav-item">
+	        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+	      </li>
+	      <li class="nav-item">
+	        <a class="nav-link" href="#">About</a>
+	      </li>
+	      <li class="nav-item dropdown active">
+	        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+	          Rivalries
+	        </a>
+	        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+	          <a class="dropdown-item" href="#">Blues vs Blackhawks</a>
+	          <a class="dropdown-item" href="#">Rangers vs Islanders</a>
+	          <a class="dropdown-item" href="#">Penguins vs Capitals</a>
+	          <a class="dropdown-item" href="#">Canadiens vs Maple Leafs</a>
+	          <a class="dropdown-item" href="#">Ducks vs Kings</a>
+	          <div class="dropdown-divider"></div>
+	          <a class="dropdown-item" href="#">Something else here</a>
+	        </div>
+	      </li>
+	    </ul>
+	    <form class="form-inline my-2 my-lg-0">
+	      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+	      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+	    </form>
+	  </div>
+	</nav>
+
+
+	<div class="container">
+
+		<!-- put your specific rivalry in header -->
+		<h1 class="text-center">Blues vs Blackhawks Rivalry</h1>
+
+
+		<!--Put the image into src -->
+        <div class="col-md-12">
+            <img class="img-fluid mx-auto d-block" src="https://orig00.deviantart.net/b907/f/2014/109/d/c/2014_nhl_playoffs_rd_1_blues_vs__blackhawks_by_epoole88-d7f2zx3.jpg">  
+        </div>
+       
+       <div class="jumbotron">
+       	<!-- put in a small caption about the rivalry here -->
+       <h5><i>The Chicago Blackhawks and St. Louis Blues have a long, sometimes heated rivalry. It has ebbed and flowed over the years, but with both clubs among the best teams in the Western Conference in recent years, the level of competitiveness between them has increased.</i></h5>
+       </div>
+            
+
+      <div class="album py-5 bg-light">
+        <div class="container">
+
+          <div class="row">
+            <div class="col-md-3">
+              <div class="card mb-4 box-shadow">
+                <img class="card-img-top" src="https://www.iconsdb.com/icons/preview/orange/trophy-2-xxl.png" alt="Card image cap">
+                <div class="card-body">
+                	<!-- Insert here -->
+        			<h5 class="text-center"><u>Stanley Cups</u></h5>
+                  <p class="card-text text-center"><strong>BLACKHAWKS : 6</strong></p>
+                  <p class="text-muted text-center">Blues : 0</p>
+                  <div class="d-flex justify-content-between align-items-center">
+                    <div class="btn-group">
+                      
+                    </div>
+                    
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-3">
+              <div class="card mb-4 box-shadow">
+                <img class="card-img-top" src="https://t3.ftcdn.net/jpg/01/26/92/14/240_F_126921478_xKTMltPjU4fyr2LdpgCTD331fDeiSOoF.jpg" alt="Card image cap">
+                <div class="card-body">
+                	<!-- Insert here -->
+                	<h5 class="text-center"><u>All Time Record</u></h5>
+                  <p class="card-text text-center"><strong>183-143-35-11</strong></p>
+                  <p class="text-center"><strong>CHICAGO</strong></p>
+                  <div class="d-flex justify-content-between align-items-center">
+                    <div class="btn-group">
+                      
+                    </div>
+                   
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-3">
+              <div class="card mb-4 box-shadow">
+                <img class="card-img-top" src="https://hubsportsboston.com/wp-content/uploads/2016/12/hub-playoffs-prizes-icon.png" alt="Card image cap">
+                <div class="card-body">
+                	<!-- Insert here -->
+                	<h5 class="text-center"><u>Playoff Appearances</u></h5>
+                  <p class="card-text text-center"><strong>BLACKHAWKS : 62</strong></p>
+                  <p class="text-muted text-center">Blues : 41</p>
+                  <div class="d-flex justify-content-between align-items-center">
+                    <div class="btn-group">
+                      
+                    </div>
+                    
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div class="col-md-3">
+              <div class="card mb-4 box-shadow">
+                <img class="card-img-top" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQZWu74pci_Z3zuk-gtW0vROgSVjgXu9-7yy2irEKURfalLSN8o" alt="Card image cap">
+                <div class="card-body">
+                	<!-- Insert here -->
+                	<h5 class="text-center"><u>MVPs</u></h5>
+                  <p class="card-text text-center"><strong>BLACKHAWKS : 7</strong></p>
+                  <p class="text-muted text-center">Blues : 2</p>
+                  <div class="d-flex justify-content-between align-items-center">
+                    <div class="btn-group">
+                     
+                    </div>
+                   
+                  </div>
+                </div>
+              </div>
+            </div>
+            
+          </div>
+        </div>
+      </div>
+      <div class="card mb-4 box-shadow">
+		<h1 class="text-center">Current Team Statistics</h1>
+          <table class="table table-bordered table-striped table-hover">
+            <thead>
+                <tr>
+                <td class="text-center" colspan="14">Team Statistics</td>
+                </tr>
+            </thead>
+        
+            <tbody>
+                <tr>
+                    <th>Team</th>
+                    <th>City</th>
+                    <th>TeamCode</th>
+                    <th>Games Played</th>
+                    <th>Points</th>
+                    <th>Wins</th>
+                    <th>Losses</th>
+                    <th>OTL</th>
+                    <th>Division</th>
+                    <th>Conferences</th>
+                    <th>Stanley Cups</th>
+                
+                </tr>
+            <?php
+$servername = "sql303.epizy.com";
+$username = "epiz_20659252";
+$password = "Blues314";
+$dbname = "epiz_20659252_BluesHawks";
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+#echo "<h1>Connected successfully</h1>";
+$sql = "SELECT * FROM teams";
+$result = $conn->query($sql);
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "<tr>";
+echo "<td>" . $row['Team'] . "</td>" ;
+echo "<td>" . $row['City'] . "</td>" ;
+echo "<td>" . $row['TeamCode'] . "</td>" ;
+echo "<td>" . $row['GamesPlayed'] . "</td>" ;
+echo "<td>" . $row['Points'] . "</td>" ;
+echo "<td>" . $row['Wins'] . "</td>" ;
+echo "<td>" . $row['Losses'] . "</td>" ;
+echo "<td>" . $row['OvertimeLosses'] . "</td>" ;
+echo "<td>" . $row['Division'] . "</td>" ;
+echo "<td>" . $row['Conferences'] . "</td>" ;        
+echo "<td>" . $row['StanleyCups'] . "</td>" ;
+echo "</tr>";
+    }
+} else {
+    echo "<p>0 results</p>";
+} //test
+                
+$conn->close();
+?>
+                
+            </tbody>
+        </table>
+        
+        <form action="" method="post">
+        <table class="table table-bordered table-striped table-hover">
+            <thead>
+            <tr>
+            <td class="text-center" colspan="14">Blues Statistics</td>
+            </tr>
+            </thead>
+            <tbody>
+                <tr>
+                
+                    <th>Player</th>
+                    <th>GP</th>
+                    <th>G</th>
+                    <th>A</th>
+                    <th>PTS</th>
+                    <th>+/-</th>
+                    <th>PIM</th>
+                    <th>PPG</th>
+                    <th>PPP</th>
+                    <th>SHG</th>
+                    <th>SHP</th>
+                    <th>GWG</th>
+                    <th>OTG</th>
+                    <th>SHOTS</th>
+                    <th>Delete</th>
+                </tr>
+                
+<?php
+	</div>
+
+
+
+
+	
+	</div>
+
+	
+
+</body>
+</html>
